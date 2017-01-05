5fbee36d059a615dbe0bda534cb59b4a9dd8f52a
lightsalt2011 <believelf2007@gmail.com>
2017-01-05 15:34:05 +0800

    feature: add simg2img usage

--- utk.sh
+++ utk.sh
@@ -27,6 +27,27 @@ utk_mount-net()
 	warning $?
 }
 
+simg2img-help()
+{
+	echo "11\
+		file system.img
+        system.img: Linux rev 1.0 ext4 filesystem data, UUID=57f8f4bc-abf4-655f-bf67-946fc0f9f25b (needs journal recovery) (extents) (large files)
+		mount -t ext4 -o loop system.img  systemimg
+
+		./make_ext4fs -l 1024M -s -a system system_new.img systemimg
+
+		file system_new.img
+		system_new.img: data
+		./simg2img system_new.img system_out.img
+		file system_out.img
+		system_out.img: Linux rev 1.0 ext4 filesystem data
+
+
+		[run]:	simg2img /disk10/lvfeng.lf/trunk/robot/xmake/../prebuilts/images/tablet_cht/system.img /tmp/tmpiVCHcF/system.img.ext4
+		[run]:	mksquashfs /tmp/tmpiVCHcF/system.img.ext4 /tmp/tmpiVCHcF/system.sqsh
+		unsquashfs system.sqsh system.img.ext4
+		"
+}
 case "$1" in 
 	mount-net)
 		utk_mount-net
@@ -34,6 +55,14 @@ case "$1" in
 		;;
 	umount-net)
 		umount ~/mnt/flash/ # TODO
+		exit
+		;;
+	rssh)
+		ssh lvfeng.lf@10.101.169.18
+		exit
+		;;
+	simg2img)
+		simg2img-help
 		exit
 		;;
 	*) adb shell $*;;
