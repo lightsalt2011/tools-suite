684ae4fca38a4bb5bcaae6538106c9b7b1942b61
lightsalt2011 <believelf2007@gmail.com>
2017-03-21 10:49:45 +0800

    add bash_profile, gitconfig

--- ubuntu-env-config/install_vim.sh
+++ ubuntu-env-config/install_vim.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #vim
-sudo apt install vim && sudo apt install exuberant-ctags &&
+cp bash_profile ~/.bash_profile
+
+sudo apt-get install vim && sudo apt-get install exuberant-ctags &&
 tar -xzvf vim_config.tar.gz
 cp vim_config/vim_rc/.vim* ~/
 tar -xzvf vim_config/vim_dir.tar.gz -C ./vim_config
